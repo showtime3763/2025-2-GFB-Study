2d5e8ec24642670243624d845eb82580e4322338
JH-REGION\xwing <xwing114@naver.com>
2025-09-30 19:47:04 +0900

    [FEAT] 예제실습1

---
+++ app.js
@@ -0,0 +1,40 @@
+/* eslist-disable */
+
+import logo from './logo.svg';
+import './App.css';
+import {useState } from 'react';
+
+function App() {
+
+	let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+    let [따봉, 따봉변경] = useState(0);
+
+    function 함수(){
+        console.log(1);
+    }
+
+	return (
+		<div className="App">
+			<div className="black-nav">
+				<h4>ReactBlog</h4>
+			</div>
+			<div className="list">
+				<h4>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
+				<p>2월 17일 발행</p>
+			</div>
+
+
+
+            <div className="list">
+				<h4>{ 글제목[1] }</h4>
+				<p>2월 17일 발행</p>
+			</div>
+            <div className="list">
+				<h4>{ 글제목[2] }</h4>
+				<p>2월 17일 발행</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
