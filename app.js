823d7931d43b71db4ce0cb95836c3da4c9d6121b
JH-REGION\xwing <xwing114@naver.com>
2025-10-28 17:26:23 +0900

    [FEAT] 과제

--- app.js
+++ app.js
@@ -6,24 +6,30 @@ import {useState } from 'react';
 
 function App() {
 
-	let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+	let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
     let [따봉, 따봉변경] = useState(0);
-
-    function 함수(){
-        console.log(1);
-    }
+	let [modal, setModal]useState(1);
 
 	return (
 		<div className="App">
 			<div className="black-nav">
 				<h4>ReactBlog</h4>
 			</div>
+			<button onClick={()=>{
+				let copy = [...글제목];
+				copy[0] = '여자코트 추천';
+				글제목변경(copy);
+			}}>
+				글수정</button>
+			<button onClick={()=> {
+				let copy = [...글제목];
+				copy = copy.sort();
+					}}>가나다순정렬</button>
+
 			<div className="list">
 				<h4>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
 				<p>2월 17일 발행</p>
 			</div>
-
-
 
             <div className="list">
 				<h4>{ 글제목[1] }</h4>
@@ -33,8 +39,19 @@ function App() {
 				<h4>{ 글제목[2] }</h4>
 				<p>2월 17일 발행</p>
 			</div>
+			<Modal></Modal>
 		</div>
 	);
+}
+
+function Modal(){
+	return(
+		<div className="modal">
+			<h4>제목</h4>
+			<p>날짜</p>
+			<p>상세내용</p>
+		</div>
+	)
 }
 
 export default App;
